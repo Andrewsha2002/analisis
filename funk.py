e3a513a5a1c170dbe22e15acff5f47b10504769b
Andrewsha2002 <krasnykov2018@gmail.com>
2021-11-20 18:41:40 +0300

    add iteration counter

--- funk.py
+++ funk.py
@@ -1,17 +1,21 @@
-
+import time
 a = [2,4,14,7,4,75,26,463,636]
 
 def bubble(array):
-
+    st=time.time()
     l = len(array)
-
+    it = 0
     for i in range(l):
 
         for j in range(0, l-i-1):
 
             if array[j] > array[j+1]:
                 array[j],array[j+1]=array[j+1],array[j]
-
+            it+=1
+    t=time.time() - st
+    print(it)
+    print("%s" % t)
 
 bubble(a)
 print(a)
+
