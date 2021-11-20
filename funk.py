40fdd07a4d109bf7e7851e0119ed53ddcb8c8b2b
Andrewsha2002 <krasnykov2018@gmail.com>
2021-11-20 18:12:36 +0300

    rand delite buble funk add

--- funk.py
+++ funk.py
@@ -1,1 +1,18 @@
-import random
+
+a = [2,4,14,7,4,75,26,463,636]
+
+def bubble(array):
+
+    l = len(array)
+
+    for i in range(l):
+
+        for j in range(0, l-i-1):
+
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+
+bubble(a)
+print(a)
