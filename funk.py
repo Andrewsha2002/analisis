5d8fd0727dee9bae917e2f2c6215e6214900546a
andrew <krasnykov2018@gmail.com>
2021-11-27 22:01:55 +0300

    add iteration and compare counts

--- funk.py
+++ funk.py
@@ -1,6 +1,10 @@
 import time
-import random
-a = [random.randint(1,100) for i in range(100)]
+
+import matplotlib.pyplot as plt
+import numpy as np
+from random import randint
+
+
 
 def bubble(array):
     b = array
@@ -134,7 +138,7 @@ def shell_sort(array) :
     t = time.time() - st
     print('{:0.9f}'.format(t))
     return t,it,sr
-
+"""
 def quicksort(array):
     st = time.time()
     it = 0
@@ -163,3 +167,58 @@ def quicksort(array):
                 sr += 1
                 e_array.append(n)
         return quicksort(s_array) + e_array + quicksort(m_array)
+"""
+
+Allfunk = []
+
+
+def starter(a, b, funk):  # a,b - раницы диапазона случайных чисел, funk() - функция сортировки
+    Sred = []
+    sred1 = []
+    for j in range(2, 42, 2):
+        for i in range(10):
+            Array = [randint(a, b) for i in range(j)]
+            time, n, l = funk(Array)
+            if i == 0:
+                sred1.append(time)
+                sred1.append(n)
+                sred1.append(l)
+            else:
+                sred1[0] = (sred1[0] + time) / 2
+                sred1[1] = (sred1[1] + n) / 2
+                sred1[2] = (sred1[2] + l) / 2
+        Sred.append(sred1)
+        sred1 = []
+    return Sred
+
+
+def vizualize(Allfunk):
+    plt.figure(1)  # Первое окно
+    plt.subplot(211)  # Расположение графика
+    for i in range(len(Allfunk)):
+        plt.plot([Allfunk[i][j][0] for j in range(20)])
+
+    plt.figure(2)
+    plt.subplot(211)  # Расположение графика
+    for i in range(len(Allfunk)):
+        plt.plot([Allfunk[i][j][1] for j in range(20)])
+
+    plt.figure(3)
+    plt.subplot(211)  # Расположение графика
+    for i in range(len(Allfunk)):
+        plt.plot([Allfunk[i][j][2] for j in range(20)])
+
+    plt.show()
+
+
+#########################
+funk = lambda Array: bubble(Array)
+Allfunk.append(starter(-100, 100, funk))
+funk = lambda Array: bubble_plus(Array)
+Allfunk.append(starter(-100, 100, funk))
+funk = lambda Array: vstavka(Array)
+Allfunk.append(starter(-100, 100, funk))
+####################
+
+print(Allfunk)
+vizualize(Allfunk)
