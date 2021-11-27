56b4c7f2d57b0f1a1d1e2f328d9a38b5e544aa92
andrew <krasnykov2018@gmail.com>
2021-11-27 21:07:05 +0300

    shell sort

--- funk.py
+++ funk.py
@@ -52,17 +52,21 @@ def shaker(array):
     right = len(array) - 1
     st = time.time()
     it = 0
+    sr = 0
     while left <= right:
+        it += 1
         for i in range(left, right, +1):
+
             if array[i] > array[i + 1]:
                 array[i], array[i + 1] = array[i + 1], array[i]
-                it += 1
+                sr += 1
         right -= 1
 
         for i in range(right, left, -1):
+
             if array[i - 1] > array[i]:
                 array[i], array[i - 1] = array[i - 1], array[i]
-                it += 1
+                sr += 1
         left += 1
     t = time.time() - st
     print('{:0.9f}'.format(t))
@@ -73,12 +77,16 @@ def shaker(array):
 def vibor(array):
     st = time.time()
     it = 0
+    sr = 0
     for i in range(0, len(array) - 1):
         min = i
+        it += 1
         for j in range(i + 1, len(array)):
+            it += 1
             if array[j] < array[min]:
+                sr += 1
                 min = j
-                it += 1
+
         array[i], array[min] = array[min], array[i]
     t = time.time() - st
     print('{:0.9f}'.format(t))
@@ -88,10 +96,12 @@ def vibor(array):
 def vstavka(array):
     st = time.time()
     it = 0
+    sr = 0
     for i in range(1, len(array)):
         temp = array[i]
         j = i - 1
         while (j >= 0 and temp < array[j]):
+            sr += 1
             array[j + 1] = array[j]
             j = j - 1
         array[j + 1] = temp
@@ -105,17 +115,22 @@ def shell_sort(array) :
     st = time.time()
     it = 0
     step = len(array) // 2
+    sr = 0
     while step > 0:
+        sr += 1
         for i in range(step, len(array) ):
             j = i
             delta = j - step
             while delta >= 0 and array[delta] > array[j]:
+
                 array[delta], array[j] = array[j], array[delta]
                 j = delta
                 delta = j - step
+                it += 1
+                sr += 2
         step //= 2
 
     t = time.time() - st
     print('{:0.9f}'.format(t))
-    return array
+    return t,it,sr
 
